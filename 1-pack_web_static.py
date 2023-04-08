692b1d1e3377177705c1c9185677b147a5de9fc2
Dafluxtech <dayodeji31@yahoo.com>
2023-04-08 06:39:12 +0000

    1. Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""a Fabric script that generates a .tgz archive."""
+"""A module for Fabric script that generates a .tgz archive."""
 import os
 from datetime import datetime
 from fabric.api import local, runs_once
 
 
+@runs_once
 def do_pack():
-    """
-    Creates a .tgz archive of the contents of the web_static folder and stores it in the versions directory.
-    """
-if not os.path.isdir("versions"):
+    """Archives the static files."""
+    if not os.path.isdir("versions"):
         os.mkdir("versions")
     d_time = datetime.now()
     output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
@@ -21,10 +20,10 @@ if not os.path.isdir("versions"):
         d_time.second
     )
     try:
-        local("mkdir -p versions")
-        timestamp = time.strftime("%Y%m%d%H%M%S")
-        archive_path = "versions/web_static_{}.tgz".format(timestamp)
-        local("tar -cvzf {} web_static".format(archive_path))
-        return archive_path
-    except:
-        return None
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, size))
+    except Exception:
+        output = None
+    return output
